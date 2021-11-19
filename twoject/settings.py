63e47e4b9a690456bc9a8bd445127f6b88d3a0bc
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-11-19 08:40:42 +0300

    Heroku Deployment

--- twoject/settings.py
+++ twoject/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-
+#################################
 ALLOWED_HOSTS = []
 
 
